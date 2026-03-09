d6b4038033c478fe125c91659db4cfd68877bf5c
Fernando8980 <fernando.soares3@portalsesisp.org.br>
2026-03-09 08:39:54 -0300

    init

---
+++ app.py
@@ -0,0 +1,192 @@
+from datetime import datetime
+
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, logout_user, login_user
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+
+from database import db_session, Funcionario
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'senai_terapia_sp'
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = select(Funcionario).where(Funcionario.id == int(user_id))
+    resultado = db_session.execute(user).scalar_one_or_none()
+    return resultado
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['form-email']
+        senha = request.form['form-senha']
+
+        if email and senha:
+            verificar_email = select(Funcionario).where(Funcionario.email == email)
+            resultado_email = db_session.execute(verificar_email).scalar_one_or_none()
+            if resultado_email:
+                # se encontrado na base de dados
+                if resultado_email.check_password(senha):
+                    # login correto
+                    login_user(resultado_email)
+                    flash(f'Login feito com sucesso', 'success')
+                    return redirect(url_for('home'))
+                else:
+                    # login incorreto
+                    flash('Senha incorreta', 'alert-danger')
+                    # render_templete : carrega a pagina html
+                    # redirect: com o url_for ele chama a função e ela carrega a pagina
+                    return redirect(url_for('login'))
+
+            else:
+                # se nao encontrado na base de dados
+                flash(f'Email nao encontrado', 'alert-danger')
+
+    return render_template('login.html')
+
+
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    flash('Logout sucesso', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/calculos')
+def calculos():
+    return render_template("calculos.html")
+
+
+@app.route('/operacoes')
+def operacoes():
+    return render_template("operacoes.html")
+
+
+@app.route('/somar', methods=['GET', 'POST'])
+def somar():
+    if request.method == 'POST':
+        if request.form['form-n1'] and request.form['form-n2']:
+            n1 = int(request.form['form-n1'])
+            n2 = int(request.form['form-n2'])
+            soma = n1 + n2
+            flash("Soma realizada", "alert-success")
+            return render_template("operacoes.html", n1=n1, n2=n2, soma=soma)
+        else:
+            # passo 1 :Repetir a msg e a categoria do Flash
+            flash('preencha o campo para realizar a soma', 'alert-danger')
+    return render_template("operacoes.html")
+
+
+@app.route('/subtrair', methods=['GET', 'POST'])
+def subtrair():
+    if request.method == 'POST':
+        if request.form['form-n1'] and request.form['form-n2']:
+            n1 = int(request.form['form-n1'])
+            n2 = int(request.form['form-n2'])
+            subtrair = n1 - n2
+            flash("Subtração realizada", "alert-success")
+            return render_template("operacoes.html", n1=n1, n2=n2, subtrair=subtrair)
+        else:
+            # passo 1 :Repetir a msg e a categoria do Flash
+            flash('preencha o campo para realizar a subtração', 'alert-danger')
+    return render_template("operacoes.html")
+
+
+@app.route('/multiplicar', methods=['GET', 'POST'])
+def multiplicar():
+    if request.method == 'POST':
+        if request.form['form-n1'] and request.form['form-n2']:
+            n1 = int(request.form['form-n1'])
+            n2 = int(request.form['form-n2'])
+            multiplicar = n1 * n2
+            return render_template("operacoes.html", n1=n1, n2=n2, multiplicar=multiplicar)
+
+    return render_template("operacoes.html")
+
+
+@app.route('/dividir', methods=['GET', 'POST'])
+def dividir():
+    if request.method == 'POST':
+        if request.form['form-n1'] and request.form['form-n2']:
+            n1 = int(request.form['form-n1'])
+            n2 = int(request.form['form-n2'])
+            dividir = n1 / n2
+            return render_template("operacoes.html", n1=n1, n2=n2, dividir=dividir)
+
+    return render_template("operacoes.html")
+
+
+@app.route('/funcionarios')
+def funcionarios():
+    _funcionarios_sql = select(Funcionario)
+    _funcionarios_resultado = db_session.execute(_funcionarios_sql).scalars().all()
+    return render_template("funcionarios.html" , lista_funcionarios=_funcionarios_resultado)
+
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro_funcionario():
+    if request.method == 'POST':
+        nome = request.form.get('form-nome')
+        data_nascimento = request.form.get('form-data')
+        cpf = request.form.get('form-cpf')
+        email = request.form.get('form-email')
+        senha = request.form.get('form-senha')
+        cargo = request.form.get('form-cargo')
+        print("oi")
+        salario = request.form.get('form-salario')
+        data_convertida =datetime.strptime(data_nascimento,'%Y-%m-%d')
+        if not nome or not email or not senha or not data_nascimento or not cpf or not cargo or not salario:
+            flash(f'Preencher todos os campos', 'danger')
+            return render_template('cadastro.html')
+        verifica_email = select(Funcionario).where(Funcionario.email == email)
+        verifica_cpf = select(Funcionario).where(Funcionario.cpf == cpf)
+        existe_email = db_session.execute(verifica_email, ).scalar_one_or_none()
+        existe_cpf = db_session.execute(verifica_cpf, ).scalar_one_or_none()
+        if existe_email:
+            flash(f'Email {email} ja existente', 'danger')
+            return render_template('cadastro.html')
+        if existe_cpf:
+            flash(f'CPF {cpf} ja existente ', 'danger')
+            return render_template('cadastro.html')
+        try:
+            novo_usuario = Funcionario(nome=nome, email=email, cpf=cpf, cargo=cargo, salario=float(salario),
+                                       data_nascimento=data_convertida)
+            novo_usuario.set_password(senha)
+            db_session.add(novo_usuario)
+            db_session.commit()
+            flash(f'Funcionario {nome} cadastrado com sucesso', 'success')
+            return redirect(url_for('funcionarios'))
+        except SQLAlchemyError as e:
+            flash(f'Erro na base de dados ao cadastrar funcionario', 'danger')
+            print(f'Error na base de dados: {e}')
+            return render_template('cadastro.html')
+        except Exception as e:
+            flash(f'Erro ao cadastrar funcionario', 'danger')
+            print(f'Error ao cadastrar: {e}')
+            return render_template('cadastro.html')
+    return render_template('cadastro.html')
+
+
+# TODO Final do código
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
